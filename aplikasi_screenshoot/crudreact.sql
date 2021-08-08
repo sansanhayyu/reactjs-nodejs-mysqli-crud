73d3b125bb2cdd2ebf56b65c60281546f6d82ef0
sansanhayyu <sansanhayyu@gmail.com>
2021-08-08 22:08:55 +0700

    adding files

---
+++ aplikasi_screenshoot/crudreact.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 08, 2021 at 04:56 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crudreact`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie_reviews`
+--
+
+CREATE TABLE `movie_reviews` (
+  `id` int(11) NOT NULL,
+  `movieName` varchar(200) NOT NULL,
+  `movieReview` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `movie_reviews`
+--
+
+INSERT INTO `movie_reviews` (`id`, `movieName`, `movieReview`) VALUES
+(18, 'Sam and Cat', 'Funny movie forever'),
+(19, 'Harry Potter', 'The best movie');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `movie_reviews`
+--
+ALTER TABLE `movie_reviews`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `movie_reviews`
+--
+ALTER TABLE `movie_reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
